310b2917ef9aad1229f1eda5c4a278d1df853e09
Alex Davis <alex2006hw@gmail.com>
2016-01-15 03:06:38 -0800

    update-port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var kraken = require('kraken-js'),
     session = require('express-session'),
     lusca = require('lusca'),
     options = require('./lib/spec')(),
-    port = process.env.PORT || 8000;
+    port = process.env.PORT || 3010;
 
 
 app.use(kraken(options));
