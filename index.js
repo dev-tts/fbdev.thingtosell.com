8a55cb74f6eeaaca94139adc34becb0c55ae2348
Alex Davis <alex2006hw@gmail.com>
2016-01-15 03:02:47 -0800

    update-xs-frame-allow

--- index.js
+++ index.js
@@ -3,11 +3,29 @@
 
 var kraken = require('kraken-js'),
     app = require('express')(),
+    session = require('express-session'),
+    lusca = require('lusca'),
     options = require('./lib/spec')(),
-    port = process.env.PORT || 3010;
+    port = process.env.PORT || 8000;
 
 
 app.use(kraken(options));
+
+//this or other session management will be required
+app.use(session({
+    secret: 'abc',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(lusca({
+    csrf: true,
+    csp: { /* ... */},
+    xframe: false,
+    p3p: 'ABCDEF',
+    hsts: {maxAge: 31536000, includeSubDomains: true, preload: true},
+    xssProtection: true
+}));
 
 app.listen(port, function (err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
